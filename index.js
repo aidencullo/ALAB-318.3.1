34fe5173e7bfca927afa64cc36bf966e71cb6dd8
Aiden Cullo <aidencullo@gmail.com>
2024-09-06 11:12:17 -0400

    move error handling to middleware.js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const { logTime, logMethod } = require("./middleware");
+const {
+  logTime,
+  logMethod,
+  errorHandler,
+} = require("./middleware");
 
 app.use(logTime);
 app.use(logMethod);
 
 app.get("/", (req, res, next) => {
-  return next(new Error('Error!'))
   res.send("Hello World!");
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- middleware.js
+++ middleware.js
@@ -7,3 +7,8 @@ export const logMethod = (req, res, next) => {
   console.log("Method: ", req.method);
   next();
 };
+
+export const errorHandler = (err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+}
