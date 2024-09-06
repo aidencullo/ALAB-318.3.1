068bcaf6854ce83fbb9917cf6ca8a70dbd33e3fc
Aiden Cullo <aidencullo@gmail.com>
2024-09-06 11:09:26 -0400

    Create and use error-handling middleware.

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const { logTime, logMethod } = require("./middleware");
 app.use(logTime);
 app.use(logMethod);
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
+  return next(new Error('Error!'))
   res.send("Hello World!");
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
