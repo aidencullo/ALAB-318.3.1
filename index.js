f30c59601c8807e3a112235f1ce718ea7ed129ba
Aiden Cullo <aidencullo@gmail.com>
2024-09-06 09:57:15 -0400

    Create and use at least two pieces of custom middleware.

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Request Type: ", req.method);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
